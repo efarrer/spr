8e33bf5d26c9cf15bf4c35cdfb1d013290d81f81
Evan Farrer <Evan.Farrer@gmail.com>
2025-04-10 09:23:20 -0600

    Set pr sets as the default (for this repo)
    commit-id:6ae54611

--- config/config_test.go
+++ config/config_test.go
@@ -42,6 +42,7 @@ func TestDefaultConfig(t *testing.T) {
 			LogGitHubCalls:   false,
 			StatusBitsHeader: true,
 			StatusBitsEmojis: true,
+			PRSetWorkflows:   true,
 		},
 		State: &InternalState{
 			MergeCheckCommit:      map[string]string{},
